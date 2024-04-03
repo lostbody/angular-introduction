3cfad2eb2711367f3144c64b98eafa657de1fd13
G. Voumvoulaki <g.voumvoulaki@gmail.com>
2024-04-03 22:27:04 +0300

    component-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Gina';
+
+  person0 = {
+    givenName: 'Gina',
+    surName: 'Voumvoulaki',
+    age: 36,
+    email: 'g.voumvoulaki@gmail.com',
+    address: 'Athens, Greece',
+  };
+
+  person1 = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA',
+  };
 }
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
@@ -8,10 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './person-table.component.css',
 })
 export class PersonTableComponent {
-  person = {
-    givenName: 'Gina',
-    surName: 'Voumvoulaki',
-    age: 36,
-    email: 'g.voumvoulaki@gmail.com',
-  };
+  @Input() person: Person | undefined;
 }
