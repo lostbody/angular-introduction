edb2168566c0d33ef5476312c194d10a1ddad8d2
lostbody <g.voumvoulaki@gmail.com>
2024-04-13 22:43:49 +0300

    http client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,18 @@ import { SimpleDatatableExampleComponent } from './components/simple-datatable-e
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
+import { HttpClientExampleComponent } from './components/http-client-example/http-client-example.component';
 
 export const routes: Routes = [
-  { path: 'event-bind-example', component: EventBindExampleComponent },
-  { path: '', component: WelcomeComponent },
   {
     path: 'component-input-example',
     component: ComponentInputExampleComponent,
   },
-  { path: 'for-directive-example', component: ForDirectiveExampleComponent },
+  {
+    path: 'for-directive-example',
+    component: ForDirectiveExampleComponent,
+  },
+  { path: 'event-bind-example', component: EventBindExampleComponent },
   {
     path: 'simple-datatable-example',
     component: SimpleDatatableExampleComponent,
@@ -32,4 +35,9 @@ export const routes: Routes = [
     path: 'reactive-form-example',
     component: ReactiveFormExampleComponent,
   },
+  {
+    path: 'http-client-example',
+    component: HttpClientExampleComponent,
+  },
+  { path: '', component: WelcomeComponent },
 ];
