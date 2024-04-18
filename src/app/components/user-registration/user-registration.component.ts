737fc1e9ba96992e431cc800f4670f7bbfda888b
G. Voumvoulaki <g.voumvoulaki@gmail.com>
2024-04-19 01:23:22 +0300

    user registration component primer

---
+++ src/app/components/user-registration/user-registration.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-user-registration',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './user-registration.component.html',
+  styleUrl: './user-registration.component.css',
+})
+export class UserRegistrationComponent {
+  userService = inject(UserService);
+
+  form = new FormGroup(
+    {
+      givenName: new FormControl('', Validators.required),
+      surName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+    },
+    this.passwordConfirmValidator,
+  );
+
+  passwordConfirmValidator(form: FormGroup) {
+    if (form.get('password').value != form.get('confirmPassword').value) {
+      form.get('confirmPassword').setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    }
+    return {};
+  }
+}
